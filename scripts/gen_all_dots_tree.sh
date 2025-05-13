4d0542ceb8425ce71a3e4b062261a2d42e1391db
Vladimir Riabchun <ferr.lambarginio@gmail.com>
2025-05-13 16:14:47 +0200

    Fix find patterns so they work with . root

--- scripts/gen_all_dots_tree.sh
+++ scripts/gen_all_dots_tree.sh
@@ -4,7 +4,7 @@ WPA="$1"
 FIXUP="$2"
 TREE="$3"
 SVF_ARGS=("$@")
-for src in $(find "$TREE" -depth -name *.ll.instr); do
+for src in $(find "$TREE" -depth -name "*.ll.instr"); do
 	fname="${src%.ll.instr}.dot"
 	$WPA "${SVF_ARGS[@]:3}" -ind-call-limit 1000000000 -node-alloc-strat=dense -dump-callgraph "$src"
 	rm -f callgraph_initial.dot
--- scripts/run_pass_tree.sh
+++ scripts/run_pass_tree.sh
@@ -3,7 +3,7 @@
 PASS_DIR="$1"
 TREE="$2"
 
-for file in $(find "$TREE" -depth -name *.ll); do
+for file in $(find "$TREE" -depth -name "*.ll"); do
   opt-14 -enable-new-pm=0 -load="$PASS_DIR/purge_stores.so" -remove-store -S -o "$file.instr" $file
   opt-14 -enable-new-pm=0 -load="$PASS_DIR/ir_instr.so" -instr -S -o "$file.instr" "$file.instr"
 done
